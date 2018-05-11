1271cf72f9c8b1859c6a0f712c5b631173e2ca16
Jojo <joanna96massey@gmail.com>
2018-05-11 14:08:59 +0100

    solution added

---
+++ FYPeComm/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FYPeComm.Models;
+
+namespace FYPeComm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /*----------------------------------------------These need deleting when cleaning up
+         * public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }*/
+
+        public IActionResult ProductSelection()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FYPeComm/Controllers/ProductDetailController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FYPeComm.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        public IActionResult ViewProduct()
+        {
+            return View();
+        }
+    }
+}
